84204918a08bfdb2c27156302dd3a8c7fbadcba6
Андрианов Виталий <andrianov.vital@yandex.ru>
2024-02-21 14:22:33 +0300

    HM-10 lib #4

---
+++ stm32l152ret6_HM10_Master/Core/Inc/HM-10/recieveData.h
@@ -0,0 +1,22 @@
+/*
+ * recieveData.h
+ *
+ *  Created on: 25 янв. 2024 г.
+ *      Author: Andrianov Vitaly
+ */
+
+#ifndef INC_RECIEVEDATA_H_
+#define INC_RECIEVEDATA_H_
+
+#include "stm32l1xx_hal.h"
+#include "main.h"
+#include "st7789/st7789.h"
+
+#define 	rxBuf_SIZE		128
+#define		subBuf_SIZE		256
+
+extern DMA_HandleTypeDef hdma_uart4_rx;
+
+void UART4_RxCpltCallback(UART_HandleTypeDef *huart, uint16_t Size);
+
+#endif /* INC_RECIEVEDATA_H_ */
