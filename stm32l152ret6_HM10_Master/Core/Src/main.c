3c6db16bac9aa52d30fa779166bd7c73b6ce2683
kayaton <andrianov.vital@yandex.ru>
2024-03-13 01:18:37 +0300

    some fixes #11

--- stm32l152ret6_HM10_Master/Core/Src/main.c
+++ stm32l152ret6_HM10_Master/Core/Src/main.c
@@ -24,8 +24,7 @@
 #include <string.h>
 #include "HM-10/recieveData.h"
 #include "HM-10/HM10_Setup.h"
-#include "st7789/cmsis_SPI1.h"
-#include <stdio.h>
+#include "st7789/st7789_Views/st7789_Data_Views.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -384,39 +383,6 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 			}
 		}
 	}
-}
-
-void st7789_DrawStartScreen() {
-	st7789_PrintString(20, 70, BLUE_st7789, WHITE_st7789, 1, &font_11x18, 2, "Инициализация");
-	st7789_PrintString(125, 120, BLUE_st7789, WHITE_st7789, 1, &font_11x18, 2, "BLE");
-}
-
-void st7789_DrawErrScreen() {
-  st7789_FillRect(0, 0,  320, 240, WHITE_st7789);
-  st7789_PrintString(50, 90, RED_st7789, WHITE_st7789, 1, &font_11x18, 2, "Ошибка BLE");
-}
-
-void st7789_DrawBleConnScreen() {
-	st7789_FillRect(0, 0,  320, 240, WHITE_st7789);
-	st7789_PrintString(20, 70, BLUE_st7789, WHITE_st7789, 1, &font_11x18, 2, "Подключение к");
-	st7789_PrintString(105, 120, BLUE_st7789, WHITE_st7789, 1, &font_11x18, 2, "Slave");
-}
-
-void st7789_DrawDataScreen() {
-	st7789_FillRect(0, 0,  320, 240, WHITE_st7789);
-	st7789_PrintString(20, 10, CYAN_st7789, WHITE_st7789, 1, &font_11x18, 1, "Master mode");
-	st7789_PrintString(220, 10, BLACK_st7789, GREEN_st7789, 1, &font_11x18, 1, "Сопряжен");
-
-	st7789_PrintString(20, 35, BLACK_st7789, WHITE_st7789, 1, &font_11x18, 1, "Текущая темп.C:");
-	st7789_PrintString(20, 55, BLACK_st7789, WHITE_st7789, 1, &font_11x18, 1, "Минимальная темп.C:");
-	st7789_PrintString(20, 75, BLACK_st7789, WHITE_st7789, 1, &font_11x18, 1, "Максимальная темп.C:");
-
-	st7789_PrintString(20, 100, BLACK_st7789, WHITE_st7789, 1, &font_11x18, 1, "Текущий RSSI,dbm:");
-	st7789_PrintString(20, 120, BLACK_st7789, WHITE_st7789, 1, &font_11x18, 1, "Минимальный RSSI,dbm:");
-	st7789_PrintString(20, 140, BLACK_st7789, WHITE_st7789, 1, &font_11x18, 1, "Максимальная RSSI,dbm:");
-
-	st7789_PrintString(20, 165, BLACK_st7789, WHITE_st7789, 1, &font_11x18, 1, "RX/TX/Loss:");
-	st7789_PrintString(20, 185, BLACK_st7789, WHITE_st7789, 1, &font_11x18, 1, "Последнее сообщение:");
 }
 
 void transmitPing() {
